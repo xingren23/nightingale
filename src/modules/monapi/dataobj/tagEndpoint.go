a0f2cc64b9a39e6c3db6c2b0aedf419cb8b398ee
李天顺 <litianshun@meicai.cn>
2020-07-14 22:33:52 +0800

    优化缓存key

--- src/modules/monapi/dataobj/tagEndpoint.go
+++ src/modules/monapi/dataobj/tagEndpoint.go
@@ -1,12 +1,14 @@
 package dataobj
 
 import (
-	"fmt"
+	"bytes"
 	"strings"
+	"sync"
 )
 
 const (
 	EndpointKeyPrefix  = "endpoint"
+	EndpointKeyDot     = "#"
 	EndpointKeyForLock = "hawkeye_endpoint_lock"
 )
 
@@ -17,12 +19,27 @@ type TagEndpoint struct {
 	Endpoint string `json:"endpoint"`
 }
 
+var bufferPool = sync.Pool{
+	New: func() interface{} {
+		return new(bytes.Buffer)
+	},
+}
+
 func BuildKey(srvType, srvTag string) string {
-	return fmt.Sprintf("%s#%s#%s", EndpointKeyPrefix, srvType, srvTag)
+	ret := bufferPool.Get().(*bytes.Buffer)
+	ret.Reset()
+	defer bufferPool.Put(ret)
+
+	ret.WriteString(EndpointKeyPrefix)
+	ret.WriteString(EndpointKeyDot)
+	ret.WriteString(srvType)
+	ret.WriteString(EndpointKeyDot)
+	ret.WriteString(srvTag)
+	return ret.String()
 }
 
 func SplitKey(key string) (string, string) {
-	if arr := strings.Split(key, "#"); len(arr) != 0 {
+	if arr := strings.Split(key, EndpointKeyDot); len(arr) != 0 {
 		return arr[1], arr[2] // srvType, srvTag
 	}
 	return "", ""
