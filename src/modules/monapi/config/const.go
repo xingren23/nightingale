ed5c6b1a526d585c11fc439f226246e3115ca679
gaobo05 <gaobo05@meicai.cn>
2020-07-14 11:35:04 +0800

    统一管理外部接口

--- src/modules/monapi/config/const.go
+++ src/modules/monapi/config/const.go
@@ -6,3 +6,15 @@ const (
 	ALERT          = "alert"
 	JudgesReplicas = 500
 )
+
+const (
+	//根据服务树id获取服务树详情
+	OPS_GET_SRVTREE = "/srv_tree/"
+	//根据服务树id获取子孙节点
+	OPS_SRVTREE_DESCENDANTS = "/srv_tree/descendants"
+)
+
+const (
+	//获取用户详情信息
+	SSO_SEARCH_USER = "/adminuser/searchadmin"
+)
