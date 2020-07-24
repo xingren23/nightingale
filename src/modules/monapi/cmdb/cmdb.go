c07682f08a38ce2274fa8e0aa33d6c3b51120509
wangzhiguo04 <wangzhiguo04@meicai.cn>
2020-07-24 20:37:41 +0800

    refactor cmdb interface , extract icmdbreadable interface

--- src/modules/monapi/cmdb/cmdb.go
+++ src/modules/monapi/cmdb/cmdb.go
@@ -5,15 +5,18 @@ import (
 	"github.com/toolkits/pkg/logger"
 )
 
-type IEndpoint interface {
+type IEndpointReadable interface {
 	EndpointTotal(query, batch, field string) (int64, error)
 	EndpointGet(col string, val interface{}) (*dataobj.Endpoint, error)
 	EndpointGets(query, batch, field string, limit, offset int) ([]dataobj.Endpoint, error)
 	EndpointIdsByIdents(idents []string) ([]int64, error)
-
 	EndpointUnderNodeTotal(leafids []int64, query, batch, field string) (int64, error)
 	EndpointUnderNodeGets(leafids []int64, query, batch, field string, limit, offset int) ([]dataobj.Endpoint, error)
 	EndpointUnderLeafs(leafIds []int64) ([]dataobj.Endpoint, error)
+}
+
+type IEndpoint interface {
+	IEndpointReadable
 
 	Update(e *dataobj.Endpoint, cols ...string) error
 	EndpointDel(ids []int64) error
@@ -21,8 +24,7 @@ type IEndpoint interface {
 	EndpointBindings(endpointIds []int64) ([]dataobj.EndpointBinding, error)
 }
 
-type INode interface {
-	InitNode()
+type INodeReadable interface {
 	NodeGets(where string, args ...interface{}) (nodes []dataobj.Node, err error)
 	NodeGetsByPaths(paths []string) ([]dataobj.Node, error)
 	NodeByIds(ids []int64) ([]dataobj.Node, error)
@@ -31,25 +33,38 @@ type INode interface {
 	TreeSearchByPath(query string) (nodes []dataobj.Node, err error)
 	NodeGet(col string, val interface{}) (*dataobj.Node, error)
 	NodesGetByIds(ids []int64) ([]dataobj.Node, error)
-
-	CreateChild(n *dataobj.Node, name string, leaf int, note string) (int64, error)
-	Bind(n *dataobj.Node, endpointIds []int64, delOld int) error
-	Unbind(n *dataobj.Node, hostIds []int64) error
-
-	Del(n *dataobj.Node) error
-	Rename(n *dataobj.Node, name string) error
-
 	Pids(n *dataobj.Node) ([]int64, error)
 }
 
-type INodeEndpoint interface {
+type INode interface {
+	INodeReadable
+
+	InitNode()
+	CreateChild(n *dataobj.Node, name string, leaf int, note string) (int64, error)
+	Bind(n *dataobj.Node, endpointIds []int64, delOld int) error
+	Unbind(n *dataobj.Node, hostIds []int64) error
+	Del(n *dataobj.Node) error
+	Rename(n *dataobj.Node, name string) error
+}
+
+type INodeEndpointReadable interface {
 	NodeIdsGetByEndpointId(endpointId int64) ([]int64, error)
 	EndpointIdsByNodeIds(nodeIds []int64) ([]int64, error)
 	EndpointBindingsForMail(endpoints []string) []string
+}
+
+type INodeEndpoint interface {
+	INodeEndpointReadable
+
 	NodeEndpointUnbind(nid, eid int64) error
 	NodeEndpointBind(nid, eid int64) error
 }
 
+type ICmdbReadable interface {
+	IEndpointReadable
+	INodeReadable
+	INodeEndpointReadable
+}
 type ICmdb interface {
 	IEndpoint
 	INode
@@ -74,3 +89,4 @@ func GetCmdb() ICmdb {
 func RegisterCmdb(name string, cmdb ICmdb) {
 	registryCmdb[name] = cmdb
 }
+
