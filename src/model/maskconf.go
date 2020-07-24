6f4084d266f2df7acdb9e1330c48f3dbc825ff7a
wangzhiguo04 <wangzhiguo04@meicai.cn>
2020-07-24 20:18:35 +0800

    refactor maskconfGets

--- src/model/maskconf.go
+++ src/model/maskconf.go
@@ -2,11 +2,7 @@ package model
 
 import (
 	"fmt"
-	"sort"
 	"strings"
-
-	"github.com/didi/nightingale/src/modules/monapi/cmdb/dataobj"
-	"github.com/toolkits/pkg/str"
 )
 
 type Maskconf struct {
@@ -82,83 +78,6 @@ func MaskconfGets(nodeId int64, path string) ([]Maskconf, error) {
 	for i := 0; i < len(objs); i++ {
 		objs[i].NodePath = path
 	}
-
-	return objs, nil
-}
-
-func MaskconfGetUnderNode(nodeId int64) ([]Maskconf, error) {
-	if leaf == 1 {
-		var objs []Maskconf
-		err := DB["mon"].Where("nid=?", nodeId).OrderBy("id desc").Find(&objs)
-		if err != nil {
-			return nil, err
-		}
-
-		for i := 0; i < len(objs); i++ {
-			objs[i].NodePath = path
-		}
-
-		return objs, nil
-	}
-
-	var relatedNodeIds []int64
-	err := DB["mon"].Table("maskconf").Select("nid").Find(&relatedNodeIds)
-	if err != nil {
-		return nil, err
-	}
-
-	if len(relatedNodeIds) == 0 {
-		return []Maskconf{}, nil
-	}
-
-	var nodes []dataobj.Node
-	err = DB["mon"].Where("id in ("+str.IdsString(relatedNodeIds)+")").Where("id="+fmt.Sprint(nodeId)+" or path like ?",
-		path+".%").Find(&nodes)
-	if err != nil {
-		return nil, err
-	}
-
-	count := len(nodes)
-	if count == 0 {
-		return []Maskconf{}, nil
-	}
-
-	ids := make([]int64, 0, count)
-	nmap := make(map[int64]dataobj.Node, count)
-	for i := 0; i < count; i++ {
-		nmap[nodes[i].Id] = nodes[i]
-		ids = append(ids, nodes[i].Id)
-	}
-
-	var objs []Maskconf
-	err = DB["mon"].In("nid", ids).Find(&objs)
-	if err != nil {
-		return nil, err
-	}
-
-	count = len(objs)
-	for i := 0; i < count; i++ {
-		n, has := nmap[objs[i].Nid]
-		if has {
-			objs[i].NodePath = n.Path
-		}
-	}
-
-	if count == 0 {
-		return []Maskconf{}, nil
-	}
-
-	sort.Slice(objs, func(i int, j int) bool {
-		if objs[i].NodePath < objs[j].NodePath {
-			return true
-		}
-
-		if objs[i].Id > objs[j].Id {
-			return true
-		}
-
-		return false
-	})
 
 	return objs, nil
 }
