3c5972cdde76208032f66e63d715581ffa1e6158
李天顺 <litianshun@meicai.cn>
2020-08-18 21:34:38 +0800

    update

--- src/modules/monapi/http/routes/resource.go
+++ src/modules/monapi/http/routes/resource.go
@@ -1,10 +1,7 @@
 package routes
 
 import (
-	"strings"
-
 	"github.com/didi/nightingale/src/modules/monapi/cmdb"
-	"github.com/didi/nightingale/src/modules/monapi/scache"
 	"github.com/gin-gonic/gin"
 	"github.com/toolkits/pkg/errors"
 )
@@ -12,33 +9,31 @@ import (
 type resourceForm struct {
 	Nid   int64  `json:"nid"`
 	Limit int    `json:"limit"`
-	Host  string `json:"host"`
+	Query string `json:"query"`
 }
 
 type data struct {
-	Ip   string `json:"ip"`
-	Type string `json:"type"`
+	Ip    string `json:"ip"`
+	Alias string `json:"alias"`
 }
 
 func resourcePost(c *gin.Context) {
 	var f resourceForm
 	errors.Dangerous(c.ShouldBind(&f))
 
-	leafNids, err := scache.GetLeafNids(f.Nid, []int64{}, []string{}, []string{})
+	curNode, err := cmdb.GetCmdb().NodeGet("id", f.Nid)
 	errors.Dangerous(err)
 
-	endpoints, err := cmdb.GetCmdb().EndpointUnderLeafs(leafNids)
+	leafNids, err := cmdb.GetCmdb().LeafIds(curNode)
+	errors.Dangerous(err)
+
+	endpoints, _, err := cmdb.GetCmdb().EndpointUnderNodeGets(leafNids, f.Query, "", "", f.Limit, offset(c, f.Limit,
+		999))
 	errors.Dangerous(err)
 
 	list := make([]data, 0)
-	for idx, e := range endpoints {
-		if !strings.Contains(e.Ident, f.Host) {
-			continue
-		}
-		list = append(list, data{Ip: e.Ident, Type: e.Alias})
-		if idx >= f.Limit {
-			break
-		}
+	for _, e := range endpoints {
+		list = append(list, data{Ip: e.Ident, Alias: e.Alias})
 	}
 
 	renderData(c, list, nil)
