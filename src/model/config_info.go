58fcbabe49f6fa72a400c53336da2321da9980f5
huangzhiwen <huangzhiwen02@meicai.cn>
2020-09-02 20:20:45 +0800

    fix 无法筛选配置 && 无法清理过期告警屏蔽

--- src/model/config_info.go
+++ src/model/config_info.go
@@ -58,7 +58,7 @@ func ConfigInfoGet(col string, val interface{}) (*ConfigInfo, error) {
 func ConfigInfoTotal(query string) (int64, error) {
 	if query != "" {
 		q := "%" + query + "%"
-		return DB["mon"].Where("cfg_group like ? or cfg_key like ?", q).Count(new(ConfigInfo))
+		return DB["mon"].Where("cfg_group like ? or cfg_key like ?", q, q).Count(new(ConfigInfo))
 	}
 	return DB["mon"].Count(new(ConfigInfo))
 }
