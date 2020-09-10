d6649fd6739f39aa471a0050d8b7d085be446826
李天顺 <litianshun@meicai.cn>
2020-09-10 15:04:20 +0800

    自监控

--- src/toolkits/stats/init.go
+++ src/toolkits/stats/init.go
@@ -28,7 +28,7 @@ func Init(prefix string, addr ...string) {
 	} else if file.IsExist(path.Join(runner.Cwd, "etc", "address.yml")) {
 		//address.yml 存在，则使用配置文件的地址
 		port := address.GetHTTPPort("collector")
-		PushUrl = fmt.Sprintf("http://127.0.0.1:%d/api/collector/push", port)
+		PushUrl = fmt.Sprintf("http://127.0.0.1:%d/v1/push", port)
 	}
 
 	Counter = NewCounter(prefix)
